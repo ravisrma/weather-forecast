f7eee07e5833ed8be6bc51a5a61c6316ae2b3d0b
Ravi sharma <67187238+ravisrma@users.noreply.github.com>
2025-05-31 12:01:07 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "load_balancer_security_group" {
 
 resource "aws_lb_target_group" "target_group" {
   name        = "${var.app_name}-${var.app_environment}-tg"
-  port        = 80
+  port        = 4173
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = aws_vpc.aws-vpc.id
